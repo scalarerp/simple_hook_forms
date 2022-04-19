00886cf5b811f540d50abc83d036ad6529f25212
Jonas Gedrat <jonas.gedrat@fullgauge.com.br>
2022-04-19 17:34:54 -0300

    hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import FormExample1 from './FormExample1'
+
 const App = () => {
     return (
         <div className="mt-10 mx-auto max-w-3xl">
             <h1 className="font-bold text-5xl mb-5 ">
                 Simple React Hook Forms
             </h1>
+            <FormExample1 />
         </div>
     )
 }
